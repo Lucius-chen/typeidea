36f4f677bbeae1447c202a5217bb03054c5b50e9
root <525968271@qq.com>
2019-06-10 15:27:27 +0000

    增加bolg-comment-config-model

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -1,13 +1,9 @@
 # "# NOQA" 表示不需要pe8检测
-from .settings import * # NOQA
+from .base import * # NOQA
 DEBUG = True
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'student',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': 3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}   
